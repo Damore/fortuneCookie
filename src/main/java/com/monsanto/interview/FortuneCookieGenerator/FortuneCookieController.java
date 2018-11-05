605b1c1a247289ef90135351cd28a502f7800610
gabrieldamoremarciano <damore82@hotmail.com>
2018-11-05 00:04:15 -0200

    Solucao do exercicio

--- src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieController.java
+++ src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieController.java
@@ -1,5 +1,6 @@
 package com.monsanto.interview.FortuneCookieGenerator;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +13,26 @@ public class FortuneCookieController {
 	@Autowired
 	private QuoteRepository quoteRepository;
 
+	// The blocker issue was caused by injecting a prototype bean into a singleton instance.
 	@Autowired
-	private FortuneCookieBuilder fortuneCookieBuilder;
+	private ObjectFactory<FortuneCookieBuilder> fortuneCookieBuilderFactory;
 
 	@GetMapping(path = "/generateFortuneCookie", produces = { "application/json" })
 	public @ResponseBody FortuneCookie generateFortuneCookie(@RequestParam("client") String client,
 			@RequestParam("company") String company) {
 		String quote = quoteRepository.getRandomQuote();
-		FortuneCookie fortuneCookie = fortuneCookieBuilder.withClient(client).withCompany(company).withQuote(quote)
+		FortuneCookie fortuneCookie = getPrototypeInstance().withClient(client).withCompany(company).withQuote(quote)
 				.build();
 		return fortuneCookie;
+	}
+	
+	/**
+	 * Now, for each request, a new fortuneCookieBuilder is created.
+	 * 
+	 * @return the forturneCookieBuilder
+	 */
+	public FortuneCookieBuilder getPrototypeInstance() {
+		return fortuneCookieBuilderFactory.getObject();
 	}
 
 }
